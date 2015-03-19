0a5d15cff86f11d0df0ad384d362a4b3a0adc259
danpnnstt <deh5021@gmail.com>
2015-03-19 09:05:53 -0400

    JSON Edit initial commit
    JSONEdit program files

---
+++ JSONEdit/JSONEdit/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dhartenbach
+ * Date: 3/19/2015
+ * Time: 8:58 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JSONEdit
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
